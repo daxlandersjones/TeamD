5f698eeecb396b6ff85d6883931e34b9f7906976
dax.jones@okstate.edu <daxljones@outlook.com>
2020-04-30 20:37:25 -0500

    Started Google Maps

---
+++ Project_P2_Team_D/ProjectP2TeamD/ProjectP2TeamD/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  ProjectP2TeamD
+//
+//  Created by Dax Jones on 4/30/20.
+//  Copyright © 2020 Amanda Lowe. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
